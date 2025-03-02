2681b61bd525c3f6c189409602fc1f9c6b57db4b
Jack Allison <john123allison@gmail.com>
2025-03-02 16:06:31 -0500

    basic lox class set up

--- jlox/src/com/craftinginterpreters/lox/Lox.java
+++ jlox/src/com/craftinginterpreters/lox/Lox.java
@@ -9,5 +9,67 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    static boolean hadError = false;
 
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    /**
+     * Run a Lox file
+     * @param path path to the lox source code file
+     * @throws IOException
+     */
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset())) ;
+        if (hadError) System.exit(65)
+    }
+
+    /**
+     * Run the interpeter interactively in a REPL-like environment.
+     * @throws IOException
+     */
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+            hadError = false; // reset so an error doesn't kill the session
+        }
+    }
+
+    /**
+     * Scan source and produce a list of tokens
+     * @param source the lox source code, as a string
+     */
+   private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        // print tokens for now
+       for (Token token : tokens) {
+           System.out.println(token);
+       }
+   }
+
+   static void error(int line, String message) {
+       report(line, "", message);
+   }
+
+   private static void report(int line, String where, String message) {
+       System.err.println("[line " + "] Error" + where + ": " + message);
+       hadError = true;
+   }
 }
