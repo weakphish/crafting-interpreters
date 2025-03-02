4fc1a081c3f380487622d6a10863b54a1a7e4bdd
John Allison <john123allison@gmail.com>
2025-03-02 15:16:26 -0500

    stub lox class

---
+++ jlox/src/com/craftinginterpreters/lox/Lox.java
@@ -0,0 +1,13 @@
+package com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox {
+
+}
